baccdee23a19ccb5938b13a29f123f079a6d9697
Vika-meow <viktoriagnezdilova@gmail.com>
2021-02-23 15:16:36 +0700

    add annotation and logs

--- tsneVisualize.py
+++ tsneVisualize.py
@@ -9,17 +9,21 @@ def loadIds(fn):
         for line in f:
             th = line[:-1].split('\t')
             pair[int(th[0])] = th[1]
+    print('loaded ids...' + fn)
     return pair
 
 def tsne_plot_2d(label, embeddings, words=[], a=1):
+    print('start configure plot')
     plt.figure(figsize=(16, 9))
     colors = cm.rainbow(np.linspace(0, 1, 1))
     x = embeddings[:,0]
     y = embeddings[:,1]
     plt.scatter(x, y, c=colors, alpha=a, label=label)
+    print('start annotate')
     for i, word in enumerate(words):
         plt.annotate(word, alpha=0.3, xy=(x[i], y[i]), xytext=(5, 2),
                      textcoords='offset points', ha='right', va='bottom', size=10)
+    print('end annotate')
     plt.legend(loc=4)
     plt.grid(True)
     plt.savefig(label+".png", format='png', dpi=150, bbox_inches='tight')
@@ -27,6 +31,7 @@ def tsne_plot_2d(label, embeddings, words=[], a=1):
 
 def visualize():
     vectors = np.load("out_ae.npy")
+    print('loaded vectors...')
     dic_1 = loadIds("data/ru_en/ent_ids_1")
     dic_2 = loadIds("data/ru_en/ent_ids_2")
     dic_1.update(dic_2)
@@ -37,11 +42,12 @@ def visualize():
     for vec in vectors:
         embeddings.append(vec)
         words.append(dic_ids[i])
-        i+=1
-
+        i += 1
     tsne_ak_2d = TSNE(n_components=2, init='pca', n_iter=3500, random_state=32)
+    print('set settings for TSNE')
     embeddings_ak_2d = tsne_ak_2d.fit_transform(embeddings)
-    tsne_plot_2d('smth', embeddings_ak_2d, a=0.1)
+    print('get new embeddings in 2dims')
+    tsne_plot_2d('ru_en_knowledge_graph_entities_embeddings', embeddings_ak_2d, words, a=0.1)
 
 def main():
     visualize()
