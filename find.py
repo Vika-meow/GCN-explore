791c03af2fb6a39d7477a78addf6a743fa37eedf
Vika-meow <viktoriagnezdilova@gmail.com>
2021-02-23 16:08:03 +0700

    find

--- find.py
+++ find.py
@@ -30,24 +30,51 @@ def loadfile(fn, num=1):
             ret.append(tuple(x))
     return ret
 
-vec = np.load("out_ae.npy")
-print("vectors loaded")
-test_pair = loadfile("data/ru_en/ref_ent_ids", 2)
-Lvec = np.array([vec[e1] for e1, e2 in test_pair])
-Rvec = np.array([vec[e2] for e1, e2 in test_pair])
-sim = scipy.spatial.distance.cdist(Lvec, Rvec, metric='cityblock')
-print("sim counted")
-dic_1 = loadIds("data/ru_en/ent_ids_1")
-dic_2 = loadIds("data/ru_en/ent_ids_2")
-dic_1.update(dic_2)
-dic_ids = dic_1
-print("ids loaded")
+def findCloseFromDifferentLang(num=10):
+    vec = np.load("out_ae.npy")
+    print("vectors loaded")
+    test_pair = loadfile("data/ru_en/ref_ent_ids", 2)
+    Lvec = np.array([vec[e1] for e1, e2 in test_pair])
+    Rvec = np.array([vec[e2] for e1, e2 in test_pair])
+    sim = scipy.spatial.distance.cdist(Lvec, Rvec, metric='cityblock')
+    print("sim counted")
+    dic_1 = loadIds("data/ru_en/ent_ids_1")
+    dic_2 = loadIds("data/ru_en/ent_ids_2")
+    dic_1.update(dic_2)
+    dic_ids = dic_1
+    print("ids loaded")
+    while(1):
+        print("print entity id")
+        a = int(input())
+        print(dic_ids[a])
+        rank = sim[a, :].argsort()
+        rank = rank[:num]
+        for el in rank:
+            print(dic_ids[13500 + el] + "\t")
 
-while(1):
-	print("print entity id")
-	a = int(input())
-	print(dic_ids[a])
-	rank = sim[a, :].argsort()
-	rank = rank[:10]
-	for el in rank:
-		print(dic_ids[13500 + el] + "\t")
+def findCloseFromAll(num=10):
+    vec = np.load("out_ae.npy")
+    print("vectors loaded")
+    test_pair = loadfile("data/ru_en/ref_ent_ids", 2)
+    Lvec = np.copy(vec)
+    Rvec = np.copy(vec)
+    sim = scipy.spatial.distance.cdist(Lvec, Rvec, metric='cityblock')
+    print("sim counted")
+    dic_1 = loadIds("data/ru_en/ent_ids_1")
+    dic_2 = loadIds("data/ru_en/ent_ids_2")
+    dic_1.update(dic_2)
+    dic_ids = dic_1
+    print("ids loaded")
+    while(1):
+        print("print entity id")
+        a = int(input())
+        print(dic_ids[a])
+        rank = sim[a, :].argsort()
+        rank = rank[:num]
+        for el in rank:
+            print(dic_ids[el] + "\t")
+
+
+
+if __name__ == "__main__":
+    findCloseFromAll()
